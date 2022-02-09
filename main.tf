f9f088de5b83fd188f7944141720a5170ad77c37
siva2github <86321069+siva2github@users.noreply.github.com>
2022-02-09 13:02:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = "9b6ca63c-622c-43b7-aa80-84f5c609e165"
+  subscription_id = "9b6ca63c-622c-43b7-aa80-84f5c609e16"
   client_id       = "10af027d-f74b-4ca2-b4ff-70290f2dd4c0"
   client_secret   = "vMY7Q~u9EhNV31fb_PTXw25uEitITYj~0MAQh"
   tenant_id       = "ec6fc1f8-0df4-4cce-9a6b-26a8fc485299"
